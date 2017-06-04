037f40b03b370c4e8561499ae3d317151315b038
Muhammed Mishal <muhdmishal@gmail.com>
2017-06-04 15:38:51 +0530

    Curl

--- modules/custom/doa_custom/src/Controller/Scraper.php
+++ modules/custom/doa_custom/src/Controller/Scraper.php
@@ -14,9 +14,29 @@ class Scraper extends ControllerBase {
    */
   public function scraper() {
 
-    $markup =  file_get_contents("https://courttribunalfinder.service.gov.uk/courts/aberdeen-employment-tribunal");
+    $url = "https://courttribunalfinder.service.gov.uk/courts/aberdeen-employment-tribunal";
+    $ch = curl_init();
+    curl_setopt ($ch, CURLOPT_URL, $url);
+    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    $contents = curl_exec($ch);
+    if (curl_errno($ch)) {
+      echo curl_error($ch);
+      echo "\n<br />";
+      $contents = '';
+    } else {
+      curl_close($ch);
+    }
+
+    if (!is_string($contents) || !strlen($contents)) {
+    echo "Failed to get contents.";
+    $contents = '';
+    }
+
+    //echo $contents;
+
     $element = array(
-      '#markup' => $markup,
+      '#markup' => $contents,
     );
     return $element;
   }
