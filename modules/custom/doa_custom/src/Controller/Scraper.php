4e2c55f5bfbddf58de41ae85f4051b8c8e2cd86b
Muhammed Mishal <muhdmishal@gmail.com>
2017-06-04 15:25:53 +0530

    Comment

--- modules/custom/doa_custom/src/Controller/Scraper.php
+++ modules/custom/doa_custom/src/Controller/Scraper.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace Drupal\eminent_admin\Controller;
-
-use Drupal\Core\Controller\ControllerBase;
-
-/**
- * MediaAdd Class. Contains the methods for playlist/timeline/quote creation.
- */
-class MediaAdd extends ControllerBase {
-
-  /**
-   * Adds the media item to playlist/timeline depending on the parametrs.
-   */
-  public function scraper() {
-    $element = array(
-      '#markup' => 'Hello, world',
-    );
-    return $element;
-  }
-}
+// namespace Drupal\eminent_admin\Controller;
+//
+// use Drupal\Core\Controller\ControllerBase;
+//
+// /**
+//  * MediaAdd Class. Contains the methods for playlist/timeline/quote creation.
+//  */
+// class MediaAdd extends ControllerBase {
+//
+//   /**
+//    * Adds the media item to playlist/timeline depending on the parametrs.
+//    */
+//   public function scraper() {
+//     $element = array(
+//       '#markup' => 'Hello, world',
+//     );
+//     return $element;
+//   }
+// }
