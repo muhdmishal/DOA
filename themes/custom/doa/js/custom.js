a0d57493ca0de7d2e3afdb973827326bca2f61be
Muhammed Mishal <muhdmishal@gmail.com>
2017-06-07 20:24:34 +0530

    Update

--- themes/custom/doa/js/custom.js
+++ themes/custom/doa/js/custom.js
@@ -1,38 +1,38 @@
 function initMap() {
 
-  var input2 = document.getElementById('whattosearch');
+  var input = document.getElementById('edit-postcode');
 
-  var autocomplete2 = new google.maps.places.Autocomplete(input2);
+  var autocomplete = new google.maps.places.Autocomplete(input);
 
-  autocomplete2.addListener('place_changed', function() {
-    var place2 = autocomplete2.getPlace();
-    if (!place2.geometry) {
+  autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
       // User entered the name of a Place that was not suggested and
       // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place2.name + "'");
+      window.alert("No details available for input: '" + place.name + "'");
       return;
     }
-    var address2 = '';
-    if (place2.address_components) {
-      address2 = [
-        (place2.address_components[0] && place2.address_components[0].short_name || ''),
-        (place2.address_components[1] && place2.address_components[1].short_name || ''),
-        (place2.address_components[2] && place2.address_components[2].short_name || '')
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
       ].join(' ');
 
-      var address_zip2 = place2.address_components;
-      var searchPostalCode2 = "";
-      $.each(address_zip2, function(){
+      var address_zip = place.address_components;
+      var searchPostalCode = "";
+      $.each(address_zip, function(){
           if(this.types[0]=="postal_code"){
-              searchPostalCode2=this.short_name;
+              searchPostalCode=this.short_name;
           }
       });
 
-      if (searchPostalCode2 == "") {
+      if (searchPostalCode == "") {
         alert("No post code for the selected address. Please try another");
       }
       else {
-        input2.value = searchPostalCode2;
+        input.value = searchPostalCode;
       }
     }
   });
