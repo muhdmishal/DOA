d7afd02a993d23fd68c1151de955c7f2f3af364b
Muhammed Mishal <muhdmishal@gmail.com>
2017-06-07 20:58:14 +0530

    Test

--- themes/custom/doa/js/custom.js
+++ themes/custom/doa/js/custom.js
@@ -1,43 +1,46 @@
 
 (function($) {
-  function initMap() {
-
-    var input = document.getElementById('edit-postcode');
-
-    var autocomplete = new google.maps.places.Autocomplete(input);
-
-    autocomplete.addListener('place_changed', function() {
-      var place = autocomplete.getPlace();
-      if (!place.geometry) {
-        // User entered the name of a Place that was not suggested and
-        // pressed the Enter key, or the Place Details request failed.
-        console.log("No details available for input: '" + place.name + "'");
-        return;
-      }
-      var address = '';
-      if (place.address_components) {
-        address = [
-          (place.address_components[0] && place.address_components[0].short_name || ''),
-          (place.address_components[1] && place.address_components[1].short_name || ''),
-          (place.address_components[2] && place.address_components[2].short_name || '')
-        ].join(' ');
-        console.log("place" + place);
-        var address_zip = place.address_components;
-        var searchPostalCode = "";
-        console.log("Address zip" + address_zip);
-        $.each(address_zip, function(){
-            if(this.types[0]=="postal_code"){
-                searchPostalCode=this.short_name;
-            }
-        });
-
-        if (searchPostalCode == "") {
-          console.log("No post code for the selected address. Please try another");
-        }
-        else {
-          input.value = searchPostalCode;
-        }
-      }
-    });
-  }
+    // plugin code here, can use $ because it refers to our argument,
+    // not the global symbol that may not be there
 })(jQuery);
+function initMap() {
+
+  var input = document.getElementById('edit-postcode');
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+
+  autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      console.log("No details available for input: '" + place.name + "'");
+      return;
+    }
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+      console.log("place" + place);
+      var address_zip = place.address_components;
+      var searchPostalCode = "";
+      console.log("Address zip" + address_zip);
+      for each (var item in address_zip) {
+        if(item.types[0]=="postal_code"){
+            searchPostalCode=this.short_name;
+        }
+      }
+    
+
+      if (searchPostalCode == "") {
+        console.log("No post code for the selected address. Please try another");
+      }
+      else {
+        input.value = searchPostalCode;
+      }
+    }
+  });
+}
