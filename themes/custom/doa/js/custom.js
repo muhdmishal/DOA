743ec79880eb987dd9deb7330df7fc413f9c74f6
Muhammed Mishal <muhdmishal@gmail.com>
2017-06-07 21:12:11 +0530

    Input search 2

--- themes/custom/doa/js/custom.js
+++ themes/custom/doa/js/custom.js
@@ -1,8 +1,3 @@
-
-(function($) {
-    // plugin code here, can use $ because it refers to our argument,
-    // not the global symbol that may not be there
-})(jQuery);
 function initMap() {
 
   var input = document.getElementById('edit-postcode');
@@ -38,6 +33,43 @@ function initMap() {
       }
       else {
         input.value = searchPostalCode;
+      }
+    }
+  });
+
+  var input2 = document.getElementById('edit-pc');
+
+  var autocomplete2 = new google.maps.places.Autocomplete(input2);
+
+  autocomplete2.addListener('place_changed', function() {
+    var place2 = autocomplete2.getPlace();
+    if (!place2.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      console.log("No details available for input: '" + place2.name + "'");
+      return;
+    }
+    var address2 = '';
+    if (place2.address_components) {
+      address2 = [
+        (place2.address_components[0] && place2.address_components[0].short_name || ''),
+        (place2.address_components[1] && place2.address_components[1].short_name || ''),
+        (place2.address_components[2] && place2.address_components[2].short_name || '')
+      ].join(' ');
+      var address_zip2 = place2.address_components;
+      var searchPostalCode2 = "";
+      jQuery.each(address_zip2, function(){
+          if(this.types[0]=="postal_code"){
+              searchPostalCode2=this.short_name;
+          }
+      });
+
+      if (searchPostalCode2 == "") {
+        alert("No post code for the selected address. Please try another");
+        input2.value = "";
+      }
+      else {
+        input2.value = searchPostalCode2;
       }
     }
   });
