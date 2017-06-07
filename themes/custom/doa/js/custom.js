6e48f79f4c67cc58da8b48619d2ac175120a1438
Muhammed Mishal <muhdmishal@gmail.com>
2017-06-07 20:54:51 +0530

    $ chnage

--- themes/custom/doa/js/custom.js
+++ themes/custom/doa/js/custom.js
@@ -1,41 +1,43 @@
-function initMap() {
 
-  var input = document.getElementById('edit-postcode');
+(function($) {
+  function initMap() {
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
+    var input = document.getElementById('edit-postcode');
 
-  autocomplete.addListener('place_changed', function() {
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      console.log("No details available for input: '" + place.name + "'");
-      return;
-    }
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
+    var autocomplete = new google.maps.places.Autocomplete(input);
 
-      var address_zip = place.address_components;
-      var searchPostalCode = "";
-      console.log("Address zip" + address_zip);
-      $.each(address_zip, function(){
-          if(this.types[0]=="postal_code"){
-              searchPostalCode=this.short_name;
-          }
-      });
-
-      if (searchPostalCode == "") {
-        console.log("No post code for the selected address. Please try another");
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        console.log("No details available for input: '" + place.name + "'");
+        return;
       }
-      else {
-        input.value = searchPostalCode;
-      }
-    }
-  });
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+        console.log("place" + place);
+        var address_zip = place.address_components;
+        var searchPostalCode = "";
+        console.log("Address zip" + address_zip);
+        $.each(address_zip, function(){
+            if(this.types[0]=="postal_code"){
+                searchPostalCode=this.short_name;
+            }
+        });
 
-}
+        if (searchPostalCode == "") {
+          console.log("No post code for the selected address. Please try another");
+        }
+        else {
+          input.value = searchPostalCode;
+        }
+      }
+    });
+  }
+})(jQuery);
